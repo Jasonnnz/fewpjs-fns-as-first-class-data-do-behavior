99291df45a68b75a193cacf9384b559da4883b41
Jason Zhou <jzhou35@buffalo.edu>
2021-01-11 17:02:56 -0500

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
+function greet(time) {
+  let t = parseInt(time.replace(':',''));
+  if (t < 1200){
+    return "Good Morning";
+  } else if (t <= 1700 && t >= 1200){
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
 
+let greeting = document.getElementById('greeting');
+function displayMessage(message) {
+  greeting.innerText = message;
+}
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
